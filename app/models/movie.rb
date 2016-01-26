afd04a5cef9c702cf7e124c373d09fcf0ba11235
unknown <c13330621@BUS-1025-43.ict.ad.dit.ie>
2016-01-26 15:25:13 +0000

    Validations added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ActiveRecord::Base
 	belongs_to:genre, dependent: :destroy
 	
 	validates :title, :released_on, presence: true
+	
+	validate :must_have_valid_trailer
+	
+	def must_have_valid_trailer
+		unless trailer.include?("<iframe")
+			errors.add(:trailer, "Must include an iframe tag")
+		end
+	end
 end
